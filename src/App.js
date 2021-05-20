3ec4d7ee222524a9d0ec24df7af39a946d496a12
99Raul <rauljr96@gmail.com>
2021-05-19 18:41:58 -0700

    added changes to image

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
 import wordsToNumbers from 'words-to-numbers';
+import Modal from './components/Modal/Modal';
 import useStyles from './styles';
+import alanPic from './assests/alan-ai-pic.jpeg';
 
 const alanKey = process.env.REACT_APP_API_KEY;
 
 const App = () => {
 	const [newsArticles, setNewsArticles] = useState([]);
 	const [activeArticle, setActiveArticle] = useState(0);
-	// const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
+
+	// const [showFeedback, setShowFeedback] = useState(false);
 
 	const classes = useStyles();
 
@@ -19,10 +23,11 @@ const App = () => {
 			onCommand: ({ command, articles, number }) => {
 				if (command === 'newHeadlines') {
 					setNewsArticles(articles);
-				} /*else if (command === 'instructions') {
-        setIsOpen(true) } */ else if (
-					command === 'highlight'
-				) {
+				} else if (command === 'instructions') {
+					setIsOpen(true);
+				} /* else if (command === 'email') {
+					setShowFeedback(true);
+				}  */ else if (command === 'highlight') {
 					setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
 				} else if (command === 'open') {
 					const parsedNumber =
@@ -47,12 +52,14 @@ const App = () => {
 	return (
 		<div>
 			<div className={classes.logoContainer}>
-				<img
-					src='https://alan.app/voice/images/previews/preview.jpg'
-					className={classes.alanLogo}
-					alt='logo'
-				/>
+				<img src={alanPic} className={classes.alanLogo} alt='logo' />
 			</div>
+			<Modal
+				isOpen={isOpen}
+				setIsOpen={setIsOpen}
+				// showFeedback={showFeedback}
+				// setShowFeedback={setShowFeedback}
+			/>
 			<NewsCards articles={newsArticles} activeArticle={activeArticle} />
 		</div>
 	);
